2e352591982a575e892d867381b227eef9a89314
unknown <ahmadzeina821@gmail.com>
2024-04-27 14:42:58 +0300

    Initial commit

---
+++ static/react-app/static/js/react-init.js
@@ -0,0 +1,12 @@
+// static/react-app/static/js/react-init.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';  // Import your main React component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
